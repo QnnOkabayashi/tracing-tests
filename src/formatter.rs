d3f95c11e47fa125614e336d565306b4d8a210ef
Quinn Okabayashi <qokabay1@swarthmore.edu>
2021-07-14 19:29:55 -0400

    Added intermediate `Event` repr for logging

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,127 +1,116 @@
-use chrono::Utc;
-use std::fmt;
-use tracing::field::{Field, Visit};
-use tracing::{Event, Level};
-use tracing_subscriber::layer::Context;
-use tracing_subscriber::Registry;
+use crate::subscriber::MyEvent;
+use std::fmt::{self, Write as _};
+use tracing::Level;
 
+#[derive(Clone, Copy, Debug)]
 pub enum LogFmt {
     Json,
     Pretty,
 }
 
 impl LogFmt {
-    pub fn format_event<W: fmt::Write>(
-        &self,
-        writer: &mut W,
-        event: &Event,
-        ctx: &Context<Registry>,
-    ) -> fmt::Result {
-        let ts = Utc::now();
+    pub fn format_events(&self, events: Vec<MyEvent>) -> Result<String, fmt::Error> {
+        let mut writer = String::new();
+        let cap;
         match *self {
-            Self::Json => {
-                write!(
-                    writer,
-                    r#"{{"timestamp":"{ts}","level":"{level}","fields":{{"#,
-                    ts = ts.format("%b %m %H:%M:%S.%3f"),
-                    level = event.metadata().level(),
-                )?;
+            LogFmt::Json => {
+                const MIN_LENGTH: usize = 37 + 25 + 5 + 12 + 23 + 4;
+                let event_size = events.iter().map(|e| e.format_size(6, 3)).sum::<usize>();
+                cap = (events.len() * MIN_LENGTH) + event_size;
+                writer.reserve(cap);
+                for e in events {
+                    write!(
+                        writer,
+                        r#"{{"timestamp":"{}","level":"{}","fields":{{"#, // 37
+                        e.timestamp.to_rfc3339(),                         // 25
+                        e.level,                                          // 5
+                    )?;
 
-                event.record(&mut Visitor::new(
-                    writer,
-                    |w: &mut W, value: &dyn fmt::Debug| write!(w, r#""message":"{:?}""#, value),
-                    |w: &mut W, field: &Field, value: &dyn fmt::Debug| {
-                        write!(w, r#","{}":"{:?}""#, field.name(), value)
-                    },
-                ));
+                    format_fields(
+                        &mut writer,
+                        e.fields,
+                        |writer, value| write!(writer, r#""message":"{}""#, value), // 12
+                        |writer, field, value| write!(writer, r#","{}":"{}""#, field, value), // value_pad: 6
+                    )?;
 
-                write!(
-                    writer,
-                    r#"}},"target":"{target}","spans":["#,
-                    target = event.metadata().target(),
-                )?;
+                    write!(writer, r#"}},"target":"{}","spans":["#, e.target,)?; // 23
 
-                if let Some(scope) = ctx.event_scope(event) {
-                    let mut first = true;
-                    for span in scope {
-                        if first {
-                            first = false;
-                        } else {
-                            writer.write_char(',')?;
+                    if let Some(scope) = e.spans {
+                        let mut first = true;
+                        for span in scope {
+                            if first {
+                                first = false;
+                            } else {
+                                write!(writer, ",")?; // 1, we can always count this even for first because it's so small
+                            }
+                            write!(writer, r#""{}""#, span)?; // 2
                         }
-                        write!(writer, r#""{}""#, span.name())?;
+                        // span_pad: 3
                     }
+
+                    writeln!(writer, "]}}")? // 4
                 }
-                writeln!(writer, "]}}")
             }
-            Self::Pretty => {
-                write!(
-                    writer,
-                    "{ts} {level:>7} ",
-                    ts = ts.format("%b %m %H:%M:%S.%3f"),
-                    level = match *event.metadata().level() {
-                        Level::TRACE => "Trace 📍",
-                        Level::DEBUG => "Debug 🐛",
-                        Level::INFO => "Info 🔐",
-                        Level::WARN => "Warn 🚧",
-                        Level::ERROR => "Error 🚨",
-                    },
-                )?;
+            LogFmt::Pretty => {
+                const MIN_LENGTH: usize = 19 + 10 + 3 + 1;
+                let event_size = events.iter().map(|e| e.format_size(4, 1)).sum::<usize>();
+                cap = (events.len() * MIN_LENGTH) + event_size;
+                writer.reserve(cap);
+                for e in events {
+                    write!(
+                        writer,
+                        "{} {:>7} ",
+                        e.timestamp.format("%b %m %H:%M:%S.%3f"), // 19
+                        match e.level {
+                            Level::TRACE => "Trace 📍",
+                            Level::DEBUG => "Debug 🐛",
+                            Level::INFO => "Info 🔐",
+                            Level::WARN => "Warn 🚧",
+                            Level::ERROR => "Error 🚨",
+                        }, // 10, because emojis are 4 bytes
+                    )?;
 
-                if let Some(spans) = ctx.event_scope(event) {
-                    for span in spans.from_root() {
-                        write!(writer, "{}:", span.name())?;
+                    if let Some(scope) = e.spans {
+                        for span in scope {
+                            write!(writer, "{}:", span)?; // span_pad: 1
+                        }
                     }
+
+                    write!(writer, " {}: ", e.target)?; // 3
+
+                    format_fields(
+                        &mut writer,
+                        e.fields,
+                        |writer, value| write!(writer, "{}", value), // 0
+                        |writer, field, value| write!(writer, " | {}={}", field, value), // value_pad: 4
+                    )?;
+
+                    writeln!(writer)?; // 1
                 }
-
-                write!(writer, " {}: ", event.metadata().target())?;
-
-                event.record(&mut Visitor::new(
-                    writer,
-                    |w: &mut W, value: &dyn fmt::Debug| write!(w, "{:?}", value),
-                    |w: &mut W, field: &Field, value: &dyn fmt::Debug| {
-                        write!(w, " | {}={:?}", field.name(), value)
-                    },
-                ));
-
-                writeln!(writer)
             }
         }
+        Ok(writer)
     }
 }
 
-struct Visitor<'writer, W, MsgFn, KeyFn> {
-    first: bool,
-    writer: &'writer mut W,
-    on_msg: MsgFn,
-    on_key: KeyFn,
-}
-
-impl<'writer, W, MsgFn, KeyFn> Visitor<'writer, W, MsgFn, KeyFn> {
-    fn new(writer: &'writer mut W, on_msg: MsgFn, on_key: KeyFn) -> Self {
-        Visitor {
-            first: true,
-            writer,
-            on_msg,
-            on_key,
-        }
-    }
-}
-
-impl<'writer, W, MsgFn, KeyFn> Visit for Visitor<'writer, W, MsgFn, KeyFn>
+fn format_fields<MsgFn, KeyFn>(
+    writer: &mut String,
+    fields: Vec<(&'static str, String)>,
+    mut msg_fn: MsgFn,
+    mut key_fn: KeyFn,
+) -> fmt::Result
 where
-    W: fmt::Write,
-    MsgFn: FnMut(&mut W, &dyn fmt::Debug) -> fmt::Result,
-    KeyFn: FnMut(&mut W, &Field, &dyn fmt::Debug) -> fmt::Result,
+    MsgFn: FnMut(&mut String, String) -> fmt::Result,
+    KeyFn: FnMut(&mut String, &'static str, String) -> fmt::Result,
 {
-    fn record_debug(&mut self, field: &Field, value: &dyn fmt::Debug) {
-        if self.first {
-            assert!(field.name() == "message", "First field must be \"message\"");
-            self.first = false;
-            (self.on_msg)(self.writer, value)
+    let mut first = true;
+    fields.into_iter().try_for_each(|(field, value)| {
+        if first {
+            assert!(field == "message", "First field must be \"message\"");
+            first = false;
+            msg_fn(writer, value)
         } else {
-            (self.on_key)(self.writer, field, value)
+            key_fn(writer, field, value)
         }
-        .expect("Write failed")
-    }
+    })
 }
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -2,9 +2,13 @@ use std::any::TypeId;
 use std::fmt;
 use std::io;
 
+use chrono::{DateTime, Utc};
+
+use tokio::sync::mpsc::UnboundedSender;
+
 use tracing::field::{self, Field, Visit};
 use tracing::span::{Attributes, Record};
-use tracing::{Event, Id, Metadata, Subscriber};
+use tracing::{Event, Id, Level, Metadata, Subscriber};
 use tracing_subscriber::layer::{Context, Layered, SubscriberExt};
 use tracing_subscriber::registry::Registry;
 use tracing_subscriber::Layer;
@@ -18,202 +22,23 @@ pub struct MySubscriber {
 
 pub struct MyLayer {
     fmt: LogFmt,
+    log_tx: UnboundedSender<(LogFmt, Vec<MyEvent>)>,
+}
+
+#[derive(Debug)]
+pub struct MyEvent {
+    pub timestamp: DateTime<Utc>,
+    pub level: Level,
+    pub spans: Option<Vec<&'static str>>,
+    pub target: String,
+    pub fields: Vec<(&'static str, String)>,
 }
 
 impl MySubscriber {
-    pub fn json() -> Self {
+    pub fn new(fmt: LogFmt, log_tx: UnboundedSender<(LogFmt, Vec<MyEvent>)>) -> Self {
         MySubscriber {
-            inner: Registry::default().with(MyLayer { fmt: LogFmt::Json }),
+            inner: Registry::default().with(MyLayer { fmt, log_tx }),
         }
-    }
-
-    pub fn pretty() -> Self {
-        MySubscriber {
-            inner: Registry::default().with(MyLayer {
-                fmt: LogFmt::Pretty,
-            }),
-        }
-    }
-}
-
-#[derive(Default)]
-pub struct MyBuffer {
-    buf: Vec<u8>,
-}
-
-impl MyLayer {
-    fn log_event(&self, event: &Event, ctx: &Context<Registry>) {
-        let span = match ctx.event_span(event) {
-            Some(span) => span,
-            // We're not in any spans, do we still care about the log?
-            // Let's just ignore it for now and short-circuit.
-            _ => return,
-        };
-
-        // `extensions_mut` returns an `ExtensionsMut`, which is essentially a
-        // wrapping for the `AnyMap` type offered in https://docs.rs/anymap/0.12.1/anymap/
-        let mut extensions = span.extensions_mut();
-
-        let buf = extensions
-            .get_mut::<MyBuffer>()
-            .expect("Log buffer not found, this is a bug");
-
-        self.fmt
-            .format_event(buf, event, ctx)
-            .expect("Write failed");
-    }
-}
-
-macro_rules! with_labeled_event {
-    ($id:ident, $span:ident, $message:literal, {$($field:literal: $value:expr),*}, |$event:ident| $code:block) => {
-        let meta = $span.metadata();
-        let cs = meta.callsite();
-        let fs = field::FieldSet::new(&["message",$($field),*], cs);
-        let mut iter = fs.iter();
-        let message = field::display($message);
-        let v = [
-            (&iter.next().unwrap(), Some(&message as &dyn field::Value)),
-            $(
-                (&iter.next().unwrap(), Some(&$value as &dyn field::Value)),
-            )*
-        ];
-        let vs = fs.value_set(&v);
-        let $event = Event::new_child_of($id, meta, &vs);
-        $code
-    };
-}
-
-impl Layer<Registry> for MyLayer {
-    fn new_span(&self, attrs: &Attributes, id: &Id, ctx: Context<Registry>) {
-        let span = ctx.span(id).expect("Span not found, this is a bug");
-
-        let timed = {
-            struct Visitor {
-                timed: bool, // add other attrs we care about here...
-            }
-
-            impl Visit for Visitor {
-                fn record_i64(&mut self, _: &Field, _: i64) {}
-
-                fn record_u64(&mut self, _: &Field, _: u64) {}
-
-                fn record_bool(&mut self, field: &Field, value: bool) {
-                    if field.name() == "timed" {
-                        self.timed = value;
-                    }
-                }
-
-                fn record_str(&mut self, _: &Field, _: &str) {}
-
-                fn record_error(&mut self, _: &Field, _: &(dyn std::error::Error + 'static)) {}
-
-                fn record_debug(&mut self, _: &Field, _: &dyn fmt::Debug) {}
-            }
-
-            let mut visitor = Visitor { timed: false };
-            attrs.record(&mut visitor);
-            visitor.timed
-        };
-
-        let mut extensions = span.extensions_mut();
-
-        if timed && extensions.get_mut::<Timings>().is_none() {
-            extensions.insert(Timings::new());
-        }
-
-        if extensions.get_mut::<MyBuffer>().is_none() {
-            let buf = MyBuffer::default();
-            extensions.insert(buf);
-        }
-
-        // TODO: maybe add `FmtSpan` flags to choose which of these we want
-        with_labeled_event!(id, span, "[opened]", {}, |event| {
-            drop(extensions);
-            self.log_event(&event, &ctx);
-        });
-    }
-
-    fn on_event(&self, event: &Event, ctx: Context<Registry>) {
-        self.log_event(event, &ctx)
-    }
-
-    fn on_enter(&self, id: &Id, ctx: Context<Registry>) {
-        let span = ctx.span(id).expect("Span not found, this is a bug");
-
-        let mut extensions = span.extensions_mut();
-
-        if let Some(timings) = extensions.get_mut::<Timings>() {
-            timings.now_busy();
-        }
-    }
-
-    fn on_exit(&self, id: &Id, ctx: Context<Registry>) {
-        let span = ctx.span(id).expect("Span not found, this is a bug");
-
-        let mut extensions = span.extensions_mut();
-
-        if let Some(timings) = extensions.get_mut::<Timings>() {
-            timings.now_idle();
-        }
-    }
-
-    fn on_close(&self, id: Id, ctx: Context<Registry>) {
-        let span = ctx.span(&id).expect("Span not found, this is a bug");
-
-        let mut extensions = span.extensions_mut();
-        // `tracing_subscriber` also just calls the macro twice,
-        // so at least this isn't worse than that.
-        if let Some(timings) = extensions.remove::<Timings>() {
-
-
-            let busy = timings.display_busy();
-            let idle = timings.display_idle();
-            with_labeled_event!(id, span, "[closed]", {"time.busy": busy, "time.idle": idle }, |event| {
-                drop(extensions);
-                self.log_event(&event, &ctx);
-            });
-        } else {
-            with_labeled_event!(id, span, "[closed]", {}, |event| {
-                drop(extensions);
-                self.log_event(&event, &ctx);
-            });
-        }
-
-        let mut extensions = span.extensions_mut();
-
-        let buf = extensions
-            .remove::<MyBuffer>()
-            .expect("Log buffer not found, this is a bug");
-
-        match span.parent() {
-            Some(parent) => {
-                parent
-                    .extensions_mut()
-                    .get_mut::<MyBuffer>()
-                    .expect("Log buffer not found, this is a bug")
-                    .append_child(buf);
-            }
-            None => {
-                buf.to_writer(&mut io::stderr()).unwrap();
-            }
-        }
-    }
-}
-
-impl MyBuffer {
-    fn append_child(&mut self, mut child: Self) {
-        self.buf.append(&mut child.buf)
-    }
-
-    fn to_writer(self, writer: &mut impl io::Write) -> io::Result<()> {
-        writer.write_all(&self.buf[..])
-    }
-}
-
-impl fmt::Write for MyBuffer {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.buf.extend_from_slice(s.as_bytes());
-        Ok(())
     }
 }
 
@@ -264,5 +89,213 @@ impl Subscriber for MySubscriber {
         } else {
             self.inner.downcast_raw(id)
         }
+    }
+}
+
+impl MyLayer {
+    fn log_event(&self, event: &Event, ctx: &Context<Registry>) {
+        if let Some(span) = ctx.event_span(event) {
+            // The event is in a span, so we should log it.
+            span.extensions_mut()
+                .get_mut::<Vec<MyEvent>>()
+                .expect("Log buffer not found, this is a bug")
+                .push(MyEvent::new(event, ctx));
+        }
+    }
+}
+
+macro_rules! with_labeled_event {
+    ($id:ident, $span:ident, $message:literal, {$($field:literal: $value:expr),*}, |$event:ident| $code:block) => {
+        let meta = $span.metadata();
+        let cs = meta.callsite();
+        let fs = field::FieldSet::new(&["message",$($field),*], cs);
+        let mut iter = fs.iter();
+        let message = field::display($message);
+        let v = [
+            (&iter.next().unwrap(), Some(&message as &dyn field::Value)),
+            $(
+                (&iter.next().unwrap(), Some(&$value as &dyn field::Value)),
+            )*
+        ];
+        let vs = fs.value_set(&v);
+        let $event = Event::new_child_of($id, meta, &vs);
+        $code
+    };
+}
+
+impl Layer<Registry> for MyLayer {
+    fn new_span(&self, attrs: &Attributes, id: &Id, ctx: Context<Registry>) {
+        let span = ctx.span(id).expect("Span not found, this is a bug");
+
+        let timed = {
+            struct Visitor {
+                timed: bool, // add other attrs we care about here...
+            }
+
+            impl Visit for Visitor {
+                fn record_bool(&mut self, field: &Field, value: bool) {
+                    if field.name() == "timed" {
+                        self.timed = value;
+                    }
+                }
+
+                fn record_debug(&mut self, _: &Field, _: &dyn fmt::Debug) {}
+            }
+
+            let mut visitor = Visitor { timed: false };
+            attrs.record(&mut visitor);
+            visitor.timed
+        };
+
+        let mut extensions = span.extensions_mut();
+
+        if timed && extensions.get_mut::<Timings>().is_none() {
+            extensions.insert(Timings::new());
+        }
+
+        if extensions.get_mut::<Vec<MyEvent>>().is_none() {
+            // arbitrarily chosen values, I'm open to feedback
+            let capacity = match *attrs.metadata().level() {
+                Level::TRACE => 512,
+                Level::DEBUG => 256,
+                Level::INFO => 128,
+                Level::WARN => 32,
+                Level::ERROR => 8,
+            };
+
+            extensions.insert(Vec::<MyEvent>::with_capacity(capacity));
+        }
+
+        // TODO: maybe add `FmtSpan` flags to choose which of these we want
+        with_labeled_event!(id, span, "[opened]", {}, |event| {
+            drop(extensions);
+            self.log_event(&event, &ctx);
+        });
+    }
+
+    fn on_event(&self, event: &Event, ctx: Context<Registry>) {
+        self.log_event(event, &ctx)
+    }
+
+    fn on_enter(&self, id: &Id, ctx: Context<Registry>) {
+        let span = ctx.span(id).expect("Span not found, this is a bug");
+
+        let mut extensions = span.extensions_mut();
+
+        if let Some(timings) = extensions.get_mut::<Timings>() {
+            timings.now_busy();
+        }
+    }
+
+    fn on_exit(&self, id: &Id, ctx: Context<Registry>) {
+        let span = ctx.span(id).expect("Span not found, this is a bug");
+
+        let mut extensions = span.extensions_mut();
+
+        if let Some(timings) = extensions.get_mut::<Timings>() {
+            timings.now_idle();
+        }
+    }
+
+    fn on_close(&self, id: Id, ctx: Context<Registry>) {
+        let span = ctx.span(&id).expect("Span not found, this is a bug");
+
+        let mut extensions = span.extensions_mut();
+        // `tracing_subscriber` also just calls the macro twice,
+        // so at least this isn't worse than that.
+        if let Some(timings) = extensions.remove::<Timings>() {
+            let busy = timings.display_busy();
+            let idle = timings.display_idle();
+            with_labeled_event!(id, span, "[closed]", { "time.busy": busy, "time.idle": idle }, |event| {
+                drop(extensions);
+                self.log_event(&event, &ctx);
+            });
+        } else {
+            with_labeled_event!(id, span, "[closed]", {}, |event| {
+                drop(extensions);
+                self.log_event(&event, &ctx);
+            });
+        }
+
+        let mut extensions = span.extensions_mut();
+
+        let event_buffer = extensions
+            .remove::<Vec<MyEvent>>()
+            .expect("Log buffer not found, this is a bug");
+
+        match span.parent() {
+            Some(parent) => {
+                parent
+                    .extensions_mut()
+                    .get_mut::<Vec<MyEvent>>()
+                    .expect("Log buffer not found, this is a bug")
+                    .extend(event_buffer);
+            }
+            None => {
+                self.log_tx
+                    .send((self.fmt, event_buffer))
+                    .expect("failed to write logs");
+                // let logs = self.fmt.format_events(event_buffer).expect("Write failed");
+
+                // eprint!("{}", logs);
+            }
+        }
+    }
+}
+
+impl MyEvent {
+    fn new(event: &Event, ctx: &Context<Registry>) -> Self {
+        let timestamp = Utc::now();
+        let level = *event.metadata().level();
+        let spans = ctx
+            .event_scope(event)
+            .map(|scope| scope.map(|span| span.name()).collect());
+        let target = event.metadata().target().to_string();
+
+        let mut fields = vec![];
+
+        struct Visitor<'a>(&'a mut Vec<(&'static str, String)>);
+
+        impl<'a> Visit for Visitor<'a> {
+            fn record_debug(&mut self, field: &Field, value: &dyn fmt::Debug) {
+                self.0.push((field.name(), format!("{:?}", value)));
+            }
+        }
+
+        event.record(&mut Visitor(&mut fields));
+
+        MyEvent {
+            timestamp,
+            level,
+            spans,
+            target,
+            fields,
+        }
+    }
+
+    pub(crate) fn format_size(&self, value_pad: usize, span_pad: usize) -> usize {
+        let span_size = self
+            .spans
+            .as_ref()
+            .map(|v| {
+                // length of span name + padding by formatter
+                v.iter().map(|span| span.len() + span_pad).sum::<usize>()
+            })
+            .unwrap_or(0);
+
+        let fields_size = self
+            .fields
+            .iter()
+            .map(|(field, value)| {
+                // length of field-value pair + padding by formatter
+                field.len() + value.len() + value_pad
+            })
+            // length of "message" is accounted for in formatters already
+            .sum::<usize>()
+            - "message".len();
+
+        let target_size = self.target.len();
+
+        span_size + fields_size + target_size
     }
 }
